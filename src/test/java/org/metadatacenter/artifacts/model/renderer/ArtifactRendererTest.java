78552da861fc6274dee747aee22445b1e7e45b44
martinjoconnor <martin@zippyrate.com>
2023-08-01 14:55:07 -0700

    Use a JSON Schema validator library in tests

--- src/test/java/org/metadatacenter/artifacts/model/renderer/ArtifactRendererTest.java
+++ src/test/java/org/metadatacenter/artifacts/model/renderer/ArtifactRendererTest.java
@@ -1,12 +1,17 @@
 package org.metadatacenter.artifacts.model.renderer;
 
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonObjectFormatVisitor;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
 import org.junit.Before;
 import org.junit.Test;
 import org.metadatacenter.artifacts.model.core.TemplateSchemaArtifact;
 import org.metadatacenter.model.ModelNodeNames;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ArtifactRendererTest
 {
@@ -25,10 +30,22 @@ public class ArtifactRendererTest
 
     ObjectNode rendering = artifactRenderer.renderTemplateSchemaArtifact(templateSchemaArtifact);
 
+    assertTrue(validateJSONSchema(rendering));
+
     assertEquals(rendering.get(ModelNodeNames.JSON_SCHEMA_SCHEMA).textValue(), ModelNodeNames.JSON_SCHEMA_SCHEMA_IRI);
 
     System.out.println(rendering.toPrettyString());
 
+  }
+
+  private boolean validateJSONSchema(ObjectNode schemaNode)
+  {
+    try {
+      JsonSchema schema = JsonSchemaFactory.byDefault().getJsonSchema(schemaNode);
+      return true;
+    } catch (ProcessingException e) {
+      return false;
+    }
   }
 
   @Test
