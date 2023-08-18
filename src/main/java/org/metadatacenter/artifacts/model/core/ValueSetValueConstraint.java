40a827124fcb7ac3b2fe07da7b02f86d48e3a661
martinjoconnor <martin@zippyrate.com>
2023-08-18 08:55:20 -0700

    more conversion to Java  records

--- src/main/java/org/metadatacenter/artifacts/model/core/ValueSetValueConstraint.java
+++ src/main/java/org/metadatacenter/artifacts/model/core/ValueSetValueConstraint.java
@@ -2,5 +2,17 @@ package org.metadatacenter.artifacts.model.core;
 
 import java.net.URI;
 
+import static org.metadatacenter.artifacts.model.core.ValidationHelper.validateStringFieldNotNull;
+import static org.metadatacenter.artifacts.model.core.ValidationHelper.validateUriFieldNotNull;
+import static org.metadatacenter.model.ModelNodeNames.VALUE_CONSTRAINTS_NAME;
+import static org.metadatacenter.model.ModelNodeNames.VALUE_CONSTRAINTS_URI;
+import static org.metadatacenter.model.ModelNodeNames.VALUE_CONSTRAINTS_VS_COLLECTION;
+
 public record ValueSetValueConstraint(String name, String valueSetCollection, URI uri, int numberOfTerms) {
+
+  public ValueSetValueConstraint {
+    validateStringFieldNotNull(this, name, VALUE_CONSTRAINTS_NAME);
+    validateStringFieldNotNull(this, valueSetCollection, VALUE_CONSTRAINTS_VS_COLLECTION);
+    validateUriFieldNotNull(this, uri, VALUE_CONSTRAINTS_URI);
+  }
 }
