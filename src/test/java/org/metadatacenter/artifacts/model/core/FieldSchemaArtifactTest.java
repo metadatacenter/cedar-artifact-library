0664357eaaa20d26cde01eea36b27e43e9dd3cbd
martinjoconnor <martin@zippyrate.com>
2023-08-25 14:23:27 -0700

    minor cleanup

--- src/test/java/org/metadatacenter/artifacts/model/core/FieldSchemaArtifactTest.java
+++ src/test/java/org/metadatacenter/artifacts/model/core/FieldSchemaArtifactTest.java
@@ -17,16 +17,15 @@ public class FieldSchemaArtifactTest
 
   @Test public void testCreateFieldSchemaWithBuilder()
   {
-    List<URI> jsonLdTypes = Collections.singletonList(URI.create("https://schema.metadatacenter.org/core/TemplateField"));
     URI jsonLdId = URI.create("https://repo.metadatacenter.org/template-fields/4455");
     URI createdBy = URI.create("http://example.com/user/1");
     URI modifiedBy = URI.create("http://example.com/user/2");
     OffsetDateTime createdOn = OffsetDateTime.now();
     OffsetDateTime lastUpdatedOn = OffsetDateTime.now();
 
-    String schemaOrgName = "My Field";
-    String schemaOrgDescription = "My Field description";
-    String schemaOrgIdentifier = "id3443";
+    String name = "My Field";
+    String description = "My Field description";
+    String identifier = "id3443";
     Version version = new Version(2, 0, 0);
     Optional<Status> status = Optional.of(Status.DRAFT);
     URI previousVersion = URI.create("https://repo.metadatacenter.org/fields/3232");
@@ -35,8 +34,8 @@ public class FieldSchemaArtifactTest
 
     FieldSchemaArtifact fieldSchemaArtifact = FieldSchemaArtifact.builder().
       withJsonLdId(jsonLdId).
-      withName(schemaOrgName).
-      withDescription(schemaOrgDescription).withIdentifier(schemaOrgIdentifier).
+      withName(name).
+      withDescription(description).withIdentifier(identifier).
       withFieldUi(FieldUi.builder().withInputType(FieldInputType.TEXTFIELD).build()).
       withCreatedBy(createdBy).withCreatedOn(createdOn).withModifiedBy(modifiedBy).withLastUpdatedOn(lastUpdatedOn).
       withVersion(version).
@@ -45,15 +44,14 @@ public class FieldSchemaArtifactTest
       withPropertyURI(propertyURI.get()).
       build();
 
-    Assert.assertEquals(jsonLdTypes, fieldSchemaArtifact.jsonLdTypes());
     Assert.assertEquals(jsonLdId, fieldSchemaArtifact.jsonLdId().get());
     Assert.assertEquals(createdBy, fieldSchemaArtifact.createdBy().get());
     Assert.assertEquals(modifiedBy, fieldSchemaArtifact.modifiedBy().get());
     Assert.assertEquals(createdOn, fieldSchemaArtifact.createdOn().get());
     Assert.assertEquals(lastUpdatedOn, fieldSchemaArtifact.lastUpdatedOn().get());
-    Assert.assertEquals(schemaOrgName, fieldSchemaArtifact.name());
-    Assert.assertEquals(schemaOrgDescription, fieldSchemaArtifact.description());
-    Assert.assertEquals(schemaOrgIdentifier, fieldSchemaArtifact.identifier().get());
+    Assert.assertEquals(name, fieldSchemaArtifact.name());
+    Assert.assertEquals(description, fieldSchemaArtifact.description());
+    Assert.assertEquals(identifier, fieldSchemaArtifact.identifier().get());
     Assert.assertEquals(version, fieldSchemaArtifact.version().get());
     Assert.assertEquals(status, fieldSchemaArtifact.status());
     Assert.assertEquals(previousVersion, fieldSchemaArtifact.previousVersion().get());
@@ -76,9 +74,9 @@ public class FieldSchemaArtifactTest
     String jsonSchemaType = "object";
     String jsonSchemaTitle = "title";
     String jsonSchemaDescription = "description";
-    String schemaOrgName = "Schema Org name";
-    String schemaOrgDescription = "Schema Org description";
-    Optional<String> schemaOrgIdentifier = Optional.of("Schema Org identifier");
+    String name = "Schema Org name";
+    String description = "Schema Org description";
+    Optional<String> identifier = Optional.of("Schema Org identifier");
     Version modelVersion = new Version(1, 6, 0);
     Optional<Version> version = Optional.of(new Version(2, 0, 0));
     Optional<Status> status = Optional.of(Status.DRAFT);
@@ -88,8 +86,8 @@ public class FieldSchemaArtifactTest
 
     FieldSchemaArtifact fieldSchemaArtifact = FieldSchemaArtifact.create(jsonLdContext, jsonLdTypes, Optional.of(jsonLdId),
       Optional.of(createdBy), Optional.of(modifiedBy), Optional.of(createdOn), Optional.of(lastUpdatedOn),
-      jsonSchemaSchemaUri, jsonSchemaType, jsonSchemaTitle, jsonSchemaDescription, schemaOrgName, schemaOrgDescription,
-      schemaOrgIdentifier, modelVersion, version, status, previousVersion, derivedFrom,
+      jsonSchemaSchemaUri, jsonSchemaType, jsonSchemaTitle, jsonSchemaDescription, name, description,
+      identifier, modelVersion, version, status, previousVersion, derivedFrom,
       FieldUi.builder().withInputType(FieldInputType.TEXTFIELD).build(),
       Optional.empty(), Optional.empty(), Collections.emptyList(),
       false, Optional.empty(), Optional.empty(), propertyURI);
@@ -105,9 +103,9 @@ public class FieldSchemaArtifactTest
     Assert.assertEquals(jsonSchemaType, fieldSchemaArtifact.jsonSchemaType());
     Assert.assertEquals(jsonSchemaTitle, fieldSchemaArtifact.jsonSchemaTitle());
     Assert.assertEquals(jsonSchemaDescription, fieldSchemaArtifact.jsonSchemaDescription());
-    Assert.assertEquals(schemaOrgName, fieldSchemaArtifact.name());
-    Assert.assertEquals(schemaOrgDescription, fieldSchemaArtifact.description());
-    Assert.assertEquals(schemaOrgIdentifier, fieldSchemaArtifact.identifier());
+    Assert.assertEquals(name, fieldSchemaArtifact.name());
+    Assert.assertEquals(description, fieldSchemaArtifact.description());
+    Assert.assertEquals(identifier, fieldSchemaArtifact.identifier());
     Assert.assertEquals(modelVersion, fieldSchemaArtifact.modelVersion());
     Assert.assertEquals(version, fieldSchemaArtifact.version());
     Assert.assertEquals(status, fieldSchemaArtifact.status());
