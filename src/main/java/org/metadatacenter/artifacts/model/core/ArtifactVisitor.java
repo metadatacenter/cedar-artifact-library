fa15625d2685f031455e4772a089155d3456dd03
Martin O'Connor <martin@zipyrate.com>
2023-12-11 16:27:14 -0800

    removed unused file

--- src/main/java/org/metadatacenter/artifacts/model/core/ArtifactVisitor.java
+++
@@ -1,8 +0,0 @@
-package org.metadatacenter.artifacts.model.core;
-
-public interface ArtifactVisitor
-{
-  void visitChildArtifact(ChildArtifact childArtifact);
-
-  void visitParentArtifact(ParentArtifact parentArtifact);
-}
