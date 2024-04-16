142814f7aa663b4976459c587ba2a6c1238921ad
Martin O'Connor <martin@zipyrate.com>
2024-04-16 11:42:51 -0700

    initial work adding specialized field type hierarchy

---
+++ src/main/java/org/metadatacenter/artifacts/model/core/FieldSchemaArtifactRecord.java
@@ -0,0 +1,75 @@
+package org.metadatacenter.artifacts.model.core;
+
+import org.metadatacenter.artifacts.model.core.fields.constraints.ValueConstraints;
+import org.metadatacenter.artifacts.model.core.ui.FieldUi;
+
+import java.net.URI;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.metadatacenter.artifacts.model.core.ValidationHelper.validateListFieldNotNull;
+import static org.metadatacenter.artifacts.model.core.ValidationHelper.validateMapFieldContainsAll;
+import static org.metadatacenter.artifacts.model.core.ValidationHelper.validateMapFieldNotNull;
+import static org.metadatacenter.artifacts.model.core.ValidationHelper.validateOptionalFieldNotNull;
+import static org.metadatacenter.artifacts.model.core.ValidationHelper.validateUiFieldNotNull;
+import static org.metadatacenter.artifacts.model.core.ValidationHelper.validateUriListFieldContainsOneOf;
+import static org.metadatacenter.model.ModelNodeNames.FIELD_SCHEMA_ARTIFACT_CONTEXT_PREFIX_MAPPINGS;
+import static org.metadatacenter.model.ModelNodeNames.FIELD_SCHEMA_ARTIFACT_TYPE_IRI;
+import static org.metadatacenter.model.ModelNodeNames.JSON_LD_CONTEXT;
+import static org.metadatacenter.model.ModelNodeNames.JSON_LD_TYPE;
+import static org.metadatacenter.model.ModelNodeNames.JSON_SCHEMA_MAX_ITEMS;
+import static org.metadatacenter.model.ModelNodeNames.JSON_SCHEMA_MIN_ITEMS;
+import static org.metadatacenter.model.ModelNodeNames.SKOS_ALTLABEL;
+import static org.metadatacenter.model.ModelNodeNames.SKOS_PREFLABEL;
+import static org.metadatacenter.model.ModelNodeNames.STATIC_FIELD_SCHEMA_ARTIFACT_CONTEXT_PREFIX_MAPPINGS;
+import static org.metadatacenter.model.ModelNodeNames.STATIC_FIELD_SCHEMA_ARTIFACT_TYPE_IRI;
+import static org.metadatacenter.model.ModelNodeNames.UI;
+import static org.metadatacenter.model.ModelNodeNames.VALUE_CONSTRAINTS;
+
+public record FieldSchemaArtifactRecord(URI jsonSchemaSchemaUri, String jsonSchemaType, String jsonSchemaTitle, String jsonSchemaDescription,
+                                 Map<String, URI> jsonLdContext, List<URI> jsonLdTypes, Optional<URI> jsonLdId,
+                                 String name, String description, Optional<String> identifier,
+                                 Version modelVersion, Optional<Version> version, Optional<Status> status,
+                                 Optional<URI> previousVersion, Optional<URI> derivedFrom,
+                                 boolean isMultiple, Optional<Integer> minItems, Optional<Integer> maxItems,
+                                 Optional<URI> propertyUri,
+                                 Optional<URI> createdBy, Optional<URI> modifiedBy,
+                                 Optional<OffsetDateTime> createdOn, Optional<OffsetDateTime> lastUpdatedOn,
+                                 FieldUi fieldUi, Optional<String> skosPrefLabel, List<String> skosAlternateLabels,
+                                 Optional<ValueConstraints> valueConstraints)
+  implements FieldSchemaArtifact
+{
+  public FieldSchemaArtifactRecord
+  {
+    validateMapFieldNotNull(this, jsonLdContext, JSON_LD_CONTEXT);
+    validateUriListFieldContainsOneOf(this, jsonLdTypes, JSON_LD_TYPE,
+      Set.of(URI.create(FIELD_SCHEMA_ARTIFACT_TYPE_IRI), URI.create(STATIC_FIELD_SCHEMA_ARTIFACT_TYPE_IRI)));
+    validateOptionalFieldNotNull(this, skosPrefLabel, SKOS_PREFLABEL);
+    validateListFieldNotNull(this, skosAlternateLabels, SKOS_ALTLABEL);
+    validateOptionalFieldNotNull(this, minItems, JSON_SCHEMA_MIN_ITEMS);
+    validateOptionalFieldNotNull(this, maxItems, JSON_SCHEMA_MAX_ITEMS);
+    validateOptionalFieldNotNull(this, propertyUri, "propertyUri"); // TODO Add to ModelNodeNames
+    validateUiFieldNotNull(this, fieldUi, UI);
+    validateOptionalFieldNotNull(this, valueConstraints, VALUE_CONSTRAINTS);
+
+    if (minItems.isPresent() && minItems.get() < 0)
+      throw new IllegalStateException("minItems must be zero or greater in element schema artifact " + name);
+
+    if (maxItems.isPresent() && maxItems.get() < 1)
+      throw new IllegalStateException("maxItems must be one or greater in element schema artifact " + name);
+
+    if (minItems.isPresent() && maxItems.isPresent() && (minItems.get() > maxItems.get()))
+      throw new IllegalStateException("minItems must be lass than maxItems in element schema artifact " + name);
+
+    if (fieldUi.isStatic())
+      validateMapFieldContainsAll(this, jsonLdContext, JSON_LD_CONTEXT, STATIC_FIELD_SCHEMA_ARTIFACT_CONTEXT_PREFIX_MAPPINGS);
+    else
+      validateMapFieldContainsAll(this, jsonLdContext, JSON_LD_CONTEXT, FIELD_SCHEMA_ARTIFACT_CONTEXT_PREFIX_MAPPINGS);
+
+    jsonLdContext = Map.copyOf(jsonLdContext);
+    jsonLdTypes = List.copyOf(jsonLdTypes);
+  }
+}
