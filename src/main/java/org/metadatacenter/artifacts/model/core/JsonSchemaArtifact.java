5c71aa3e8f2bdb28a7a06864ba1e6ebbda2a31f3
martinjoconnor <martin@zippyrate.com>
2024-07-23 10:37:02 -0700

    refactoring

--- src/main/java/org/metadatacenter/artifacts/model/core/JsonSchemaArtifact.java
+++
@@ -1,8 +0,0 @@
-package org.metadatacenter.artifacts.model.core;
-
-public interface JsonSchemaArtifact
-{
-  String internalName();
-
-  String internalDescription();
-}
--- src/main/java/org/metadatacenter/artifacts/model/core/SchemaArtifact.java
+++ src/main/java/org/metadatacenter/artifacts/model/core/SchemaArtifact.java
@@ -1,25 +1,10 @@
 package org.metadatacenter.artifacts.model.core;
 
-import java.net.URI;
 import java.util.Optional;
 
-public interface SchemaArtifact extends Artifact, JsonLdArtifact, JsonSchemaArtifact, SchemaOrgArtifact,
-  VersionedArtifact, MonitoredArtifact
+public interface SchemaArtifact
+  extends Artifact, JsonLdArtifact, SchemaOrgArtifact, VersionedArtifact, MonitoredArtifact
 {
-  String name();
-
-  String description();
-
-  Optional<String> identifier();
-
-  Optional<Version> version();
-
-  Optional<Status> status();
-
-  Optional<URI> previousVersion();
-
-  Optional<URI> derivedFrom();
-
   Optional<String> language();
 
   Optional<Annotations> annotations();
